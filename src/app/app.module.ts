0ae257fb94e68251423ce6e652159813811d0bf5
Glenn Gilmartin <ggilmartin@overstock.com>
2018-06-22 15:49:10 +0100

    SideBar Cleaned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Info } from '../pages/info/info';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    Info
   ],
   imports: [
     BrowserModule,
@@ -23,7 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    Info
   ],
   providers: [
     StatusBar,
---
+++ src/pages/info/info.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-info',
+  templateUrl: 'info.html'
+})
+export class Info {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+}
